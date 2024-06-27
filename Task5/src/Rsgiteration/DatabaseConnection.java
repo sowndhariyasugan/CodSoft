9f77086e5454918986334c040a70edb56abdb5e8
SOWND <sowndhariyasugan@gmail.com>
2024-06-27 20:50:04 +0530

    Update DatabaseConnection.java

--- Task5/src/Rsgiteration/DatabaseConnection.java
+++ Task5/src/Rsgiteration/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/registration_system";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String USER = "someone";
+    private static final String PASSWORD = "something";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
